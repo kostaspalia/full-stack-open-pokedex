11766c60969c6a4494c74115c9eee02bbc4b7e9c
kostas <kostaspalia95@hotmail.gr>
2022-09-30 11:35:39 +0200

    fixes

--- cypress/e2e/simpleTest.cy.js
+++ cypress/e2e/simpleTest.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Can enter a pokemon page', function(){
+    cy.visit('http://localhost:5000')
+    cy.get('a').eq(1).click()
+    cy.contains('chlorophyll')
+  })
 })
